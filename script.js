1d0d2db2e2494a991c142de54ad5749268df02cb
libiquits <libiquits123@gmail.com>
2022-03-28 07:12:53 -0300

    Canvas First Test

---
+++ script.js
@@ -0,0 +1,11 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillRect(100, 100, 100, 100);
+c.fillRect(100, 100, 100, 100);
+c.fillRect(100, 100, 100, 100);
+console.log(canvas);

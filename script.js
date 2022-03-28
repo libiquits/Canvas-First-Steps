7b3f24cb99385fd920f1ca00b0a6106f0a657e6f
Kauan Batista <90389758+libiquits@users.noreply.github.com>
2022-03-28 19:31:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillRect(100, 100, 100, 100);
-c.fillRect(100, 100, 100, 100);
-c.fillRect(100, 100, 100, 100);
-console.log(canvas);
+
+var x =200;
+function animate() {
+requestAnimationFrame (animate);
+
+c.beginPath();
+c.arc(x, 200, 30, 0, Math.PI * 2,false);
+c.strokeStyle = 'blue';
+c.stroke();
+
+x += 1;
+
+
+}
+
+animate();
